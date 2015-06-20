47ba36910528db52edd7859e7ff495d65134e192
Nicholas Arner <nicholasarner@gmail.com>
2015-06-20 14:50:06 -0400

    Initial project setup

---
+++ FMOscillator/FMOscillator/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  FMOscillator
+//
+//  Created by Nicholas Arner on 6/20/15.
+//  Copyright (c) 2015 Nicholas Arner. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ FMOscillator/FMOscillator/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  FMOscillator
+//
+//  Created by Nicholas Arner on 6/20/15.
+//  Copyright (c) 2015 Nicholas Arner. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
