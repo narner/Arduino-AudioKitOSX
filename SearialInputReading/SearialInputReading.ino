842ea1fa58c6046f4d2d063aab02664dc4e7a3b0
Nicholas Arner <nicholasarner@gmail.com>
2015-06-20 14:50:57 -0400

    Reading input from two potentiometers

--- SearialInputReading/SearialInputReading.ino
+++ SearialInputReading/SearialInputReading.ino
@@ -1,9 +1,28 @@
-void setup() {
-  // put your setup code here, to run once:
-
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-
-}
+   /*This Arduino sketch will read teh values of two potentiometers and a 
+     toggle switch, which will be used to control the parameters of an oscillator 
+     made with Audio Kit */
+     
+     //http://www.nickarner.com
+     //http://www.audiokitio
+   
+  
+    void setup() {
+      // initialize serial communication at 9600 bits per second:
+      Serial.begin(9600);
+    }
+    
+    
+    void loop() {
+      // read the input for the first potentiometer
+      int potentiometerOneValue = analogRead(A0);
+      // Convert the analog reading (which goes from 0 - 1023) to a range between 0 and 1
+      float voltageOne = potentiometerOneValue * (1.0 / 1023.0);
+      Serial.println(voltageOne);
+      
+      // read the input for the second potentiometer
+      int potentiometerTwoValue = analogRead(A1);
+      // Convert the analog reading (which goes from 0 - 1023) to a range between 0 and 1
+      float voltageTwo = potentiometerTwoValue * (1.0 / 1023.0);
+      Serial.println(voltageTwo);
+      
+    }
