2440b2670ae544e2c51b08fc71819d2314e2d023
Nicholas Arner <nicholasarner@gmail.com>
2015-07-03 15:33:54 -0400

    Added code to open and close a serial port. Implemented ORSSerialPort delegate methods.

--- FMOscillator/FMOscillator/ViewController.swift
+++ FMOscillator/FMOscillator/ViewController.swift
@@ -9,23 +9,55 @@
 import Cocoa
 import ORSSerial 
 
-class ViewController: NSViewController {
+class ViewController: NSViewController, ORSSerialPortDelegate {
     
-    // STEP 1 : Set up an instance variable for the instrument
+    let serialPortManager = ORSSerialPortManager.sharedSerialPortManager()
+
+    var serialPort: ORSSerialPort? {
+        didSet {
+            oldValue?.close()
+            oldValue?.delegate = nil
+            serialPort?.delegate = self
+        }
+    }
+    
+
     let instrument = AKInstrument()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        self.serialPort = ORSSerialPort(path: "/dev/cu.usbmodem1421")
+        serialPort?.baudRate = 4800
+        serialPort?.open()
         
-        popen("echo hello world > /dev/cu.usbmodem1411", "r");
+        /*Code below will be used for some sort of UI that will allow 
+        the user to select what serial port they want to use as input
         
-        // STEP 2 : Define the instrument as a simple oscillator
+        let availablePorts = ORSSerialPortManager.sharedSerialPortManager().availablePorts as! [ORSSerialPort]
+        if availablePorts.count == 0 {
+            println("No connected serial ports found. Please connect your Arduino or turn on Bluetooth..\n")
+            exit(EXIT_SUCCESS)
+        }
+        
+        println("\nPlease select a serial port: \n")
+        let availablePorts = ORSSerialPortManager.sharedSerialPortManager().availablePorts as! [ORSSerialPort]
+        var i = 0
+        for port in availablePorts {
+            println("\(i++). \(port.name)")
+        }
+
+        */
+        
+
+        
+        //AK
         let oscillator = AKOscillator()
         instrument.connect(oscillator)
         instrument.connect(AKAudioOutput(audioSource: oscillator))
         
-        // STEP 3 : Add the instrument to the orchestra and start the orchestra
+        //AK
         AKOrchestra.addInstrument(instrument)
         AKOrchestra.start()
     }
@@ -39,5 +71,32 @@ class ViewController: NSViewController {
             instrument.stop()
             sender.title = "Play Sound"
         }
+    }
+    
+    
+    
+    // MARK: - ORSSerialPortDelegate
+    
+    func serialPortWasOpened(serialPort: ORSSerialPort) {
+        print("SerialPort \(serialPort) was opened")
+    }
+    
+    func serialPortWasClosed(serialPort: ORSSerialPort) {
+        print("SerialPort \(serialPort) was closed")
+    }
+    
+    func serialPortWasRemovedFromSystem(serialPort: ORSSerialPort) {
+        self.serialPort = nil
+        print("SerialPort \(serialPort) was removed from system")
+    }
+    
+    func serialPort(serialPort: ORSSerialPort, didEncounterError error: NSError) {
+        print("SerialPort \(serialPort) encountered an error: \(error)")
+    }
+    
+    
+    override func viewDidDisappear() {
+        super.viewDidDisappear()
+        serialPort?.close()
     }
 }
