e27d85e1af4c77c0024c412f3e8ef95bae705bf7
Nicholas Arner <nicholasarner@gmail.com>
2015-08-11 20:10:26 -0400

    A folder renaming elsewhere necessitated a re-adding of the AudioKit libraries.

---
+++ FMOscillator/FMOscillator/SerialCommunicator.swift
@@ -0,0 +1,9 @@
+//
+//  SerialCommunicator.swift
+//  FMOscillator
+//
+//  Created by Nicholas Arner on 8/11/15.
+//  Copyright (c) 2015 Nicholas Arner. All rights reserved.
+//
+
+import Foundation
