2aa55e4eec46b7c4348108af2383d591c17443c0
Nicholas Arner <nicholasarner@gmail.com>
2016-02-08 18:13:03 -0500

    Removing `ORSSerialPort` as a sub-module, and adding it as a CocoaPod.

---
+++ FMOscillator/Pods/Target Support Files/ORSSerialPort/ORSSerialPort-umbrella.h
@@ -0,0 +1,10 @@
+#import <Cocoa/Cocoa.h>
+
+#import "ORSSerialPacketDescriptor.h"
+#import "ORSSerialPort.h"
+#import "ORSSerialPortManager.h"
+#import "ORSSerialRequest.h"
+
+FOUNDATION_EXPORT double ORSSerialVersionNumber;
+FOUNDATION_EXPORT const unsigned char ORSSerialVersionString[];
+
